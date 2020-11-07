107241432e047fc4f571813b2a6ded4ef96898cc
Sean <seanh@zillowgroup.com>
2020-11-06 20:38:58 -0800

    fix issue

--- data/seed.sql
+++ data/seed.sql
@@ -1,5 +1,5 @@
-INSERT INTO books (author, title, isbn, image_url, description)
+INSERT INTO books (author, title, isbn, img, description)
 VALUES ('Arthur Golden', 'Memoirs of a Geisha', '0-375-40011-7', 'https://i.imgur.com/J5LVHEL.jpg', 'book about geishas during WWII');
 
-INSERT INTO books (author, title, isbn, image_url, description)
+INSERT INTO books (author, title, isbn, img, description)
 VALUES ('Steven King', 'It', '978-1-58767-270-5', 'https://i.imgur.com/J5LVHEL.jpg', 'Book about some killer clown');
